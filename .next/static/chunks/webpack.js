e0f76ebf41ac104817c96aa1e7a116f39134d0fd
Jason Choi <kchoi2@babson.edu>
2023-11-19 22:39:10 -0800

    almost there

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1a923c33b6a765c6"; }
+/******/ 		__webpack_require__.h = function() { return "4e02e9810427cffb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
